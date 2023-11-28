6f43d5437faa2df320cadd7b4a36c36d6cbee78b
mahitoburrito <62569049+mahitoburrito@users.noreply.github.com>
2023-11-28 15:39:39 -0800

    Add files via upload

---
+++ lab00/lab00.py
@@ -0,0 +1,9 @@
+def twenty_twenty_three():
+    """Come up with the most creative expression that evaluates to 2023,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_three()
+    2023
+    """
+    return int((((88 / 2) + 859) // 100) + 2 + ((2 * 2) * (5 * 5 * 5 * 4 + 3)))
+
